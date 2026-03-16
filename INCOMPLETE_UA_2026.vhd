c92db8bab489486e19dfac22cc1356a0529ad964
sercolo111 <838585@unizar.es>
2026-03-16 19:04:28 +0100

    Añadir JAL a la UA

--- INCOMPLETE_UA_2026.vhd
+++ INCOMPLETE_UA_2026.vhd
@@ -12,7 +12,8 @@ entity UA is
 			RegWrite_WB: IN std_logic;
 			RW_WB: IN  std_logic_vector(4 downto 0);
 			MUX_ctrl_A: out std_logic_vector(1 downto 0);
-			MUX_ctrl_B: out std_logic_vector(1 downto 0)
+			MUX_ctrl_B: out std_logic_vector(1 downto 0);
+			JAL_MEM: IN std_logic -- Señal para detectar que hay un jal en la etapa de memoria
 		);
 	end UA;
 
@@ -35,16 +36,29 @@ begin
 	-- Si rs (Reg_Rs_EX) es igual a lo que se va escribir en memoria (RW_WB) y se va a escribir en memoria (RegWrite_WB) y que no hay parada (V=1) (valid_I_WB)
 	Corto_A_WB	<= '1' when ((Reg_Rs_EX = RW_WB) and (RegWrite_WB = '1') and (valid_I_WB = '1')) else '0';
 	Corto_B_WB	<= '1' when ((Reg_Rs_EX = RW_WB) and (RegWrite_WB = '1') and (valid_I_WB = '1')) else '0';
+
+	-- Cuando hay un jal se va pasando a cada registro de cada etapa el valor de PC+4 para que se guarde en el registro que se indique
+	-- lo pasa que si luego hay una operación aritmetica con ese registro hay que darle el PC+4, entonces necesitamos una señal que indique
+	-- que hay un JAL cuando esta en memoria y si ocurre se pone ese valor de PC+4 en las dos mux de anticipación que se activarán si hay un jal
+	-- en la etapa de memoria, osea es lo mismo que el corto de las instrucciones aritmeticas pero añadiendole que puede haber un jal
+	Corto_A_JAL_Mem <= '1' when ((Reg_Rs_EX = RW_MEM) and (RegWrite_MEM = '1') and (valid_I_MEM = '1') and (JAL_MEM = '1'))	else '0';
+	Corto_B_JAL_Mem <= '1' when ((Reg_Rt_EX = RW_MEM) and (RegWrite_MEM = '1') and (valid_I_MEM = '1') and (JAL_MEM = '1'))	else '0';
+
 	-- With the above signals, the input of the muxes is chosen:
 	-- input 00: corresponds to the data from the register bank
 	-- input 01: data from the Mem stage
 	-- input 10: data from the WB stage
 	-- Complete: We give an example for the Corto_A_Mem, you must add the rest of cases
-	MUX_ctrl_A <= 	"01" when (Corto_A_Mem = '1') else
+
+	-- Importante poner el Corto_JAL_MEM arriba para que tenga prioridad a Corto_A_Mem ya que este también saltará
+	-- aunque haya jal o no
+	MUX_ctrl_A <= 	"11" when (Corto_A_JAL_Mem = '1') else
+					"01" when (Corto_A_Mem = '1') else
 					"10" when (Corto_A_WB  = '1') else
                		"00";
 	-- Se pone en el 10 al reves para que no se usen los mismos registros en las operaciones aritmeticas
-	MUX_ctrl_B <= 	"01" when (Corto_B_WB = '1') else  
+	MUX_ctrl_B <= 	"11" when (Corto_B_JAL_Mem = '1') else
+					"01" when (Corto_B_WB = '1') else  
 					"10" when (Corto_B_Mem  = '1') else
                		"00";	
 end Behavioral;
